85bb53e9193ffbbe4e1038c7144baf028a237809
meenakshi <meenakshilokhande1@gmail.com>
2020-10-08 21:12:10 +0530

    Initial commit

---
+++ bg.js
@@ -0,0 +1,64 @@
+let bColor = (color) =>{
+    let clr = document.querySelector('.body');
+    clr.style.backgroundColor = color;
+}
+//red
+let rColor = document.querySelector('#red');
+ rColor.addEventListener('click',function(){
+     bColor('Red');
+  
+ });
+//Green
+ let gColor = document.querySelector('#green');
+ gColor.addEventListener('click',function(){
+     bColor('Green');
+  
+ });
+ //Yellow
+ let yColor = document.querySelector('#yellow');
+ yColor.addEventListener('click',function(){
+     bColor('Yellow');
+  
+ });
+ //Pink
+ let pColor = document.querySelector('#pink');
+ pColor.addEventListener('click',function(){
+     bColor('Pink');
+  
+ });
+ //Orange
+ let oColor = document.querySelector('#orange');
+ oColor.addEventListener('click',function(){
+     bColor('Orange');
+  
+ });
+ //black
+ let bckColor = document.querySelector('#black');
+ bckColor.addEventListener('click',function(){
+     bColor('Black');
+  
+ });
+ //blue
+ let bleColor = document.querySelector('#blue');
+ bleColor.addEventListener('click',function(){
+     bColor('Blue');
+  
+ });
+ //Grey
+ let gryColor = document.querySelector('#grey');
+ gryColor.addEventListener('click',function(){
+     bColor('Grey');
+  
+ });
+ //White
+ let wColor = document.querySelector('#crimson');
+ wColor.addEventListener('click',function(){
+     bColor('Crimson');
+  
+ });
+ //Cyon
+ let cColor = document.querySelector('#cyan');
+ cColor.addEventListener('click',function(){
+     bColor('Cyan');
+  
+ });
